e84745d01188b7869fdb56ed6c786b29218da0ec
celephicus <celephicus@gmail.com>
2022-11-14 11:39:22 +1100

    Renamed relay-l to relay and it builds.

--- Software/Relay/Relay-as/Relay-as/project_config.h
+++ Software/Relay/Relay-as/Relay-as/project_config.h
@@ -26,10 +26,10 @@ enum {
 #define CFG_VER_MINOR 0
 
 // Build number incremented with each build by cfg-set-build.py script. 
-#define CFG_BUILD_NUMBER 439
+#define CFG_BUILD_NUMBER 440
 
 // Timestamp in ISO8601 format set by cfg-set-build.py script.
-#define CFG_BUILD_TIMESTAMP "20221114T100154"
+#define CFG_BUILD_TIMESTAMP "20221114T113747"
 
 // Do not edit below this line......
 
