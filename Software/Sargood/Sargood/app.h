06e9865648ba94e4cb4c537e1a164294571cad2c
celephicus <celephicus@gmail.com>
2023-04-28 11:44:15 +1000

    Redid command codes for sending to client.

--- Software/Sargood/Sargood/app.h
+++ Software/Sargood/Sargood/app.h
@@ -21,18 +21,21 @@ enum {
 	// No-op, also flags command processor as idle.
 	APP_CMD_IDLE = 0,
 
-	// Stop motion.
-	APP_CMD_STOP = 1,
+	// Wakeup controller for a while, currently 5 secs.
+	APP_CMD_WAKEUP = 1,
+	
+	// Stop all motion.
+	APP_CMD_STOP = 2,
 
 	// Timed motion of a single axis. Error code RELAY_FAIL.
-	APP_CMD_HEAD_UP = 2,
-	APP_CMD_HEAD_DOWN = 3,
-	APP_CMD_LEG_UP = 4,
-	APP_CMD_LEG_DOWN = 5,
-	APP_CMD_BED_UP = 6,
-	APP_CMD_BED_DOWN = 7,
-	APP_CMD_TILT_UP = 8,
-	APP_CMD_TILT_DOWN = 9,
+	APP_CMD_HEAD_UP = 101,
+	APP_CMD_HEAD_DOWN = 102,
+	APP_CMD_LEG_UP = 103,
+	APP_CMD_LEG_DOWN = 104,
+	APP_CMD_BED_UP = 105,
+	APP_CMD_BED_DOWN = 106,
+	APP_CMD_TILT_UP = 107,
+	APP_CMD_TILT_DOWN = 108,
 
 	// Save current position as a preset. Error codes SENSOR_FAIL_x.
 	APP_CMD_SAVE_POS_1 = 100,
@@ -50,17 +53,18 @@ enum {
 // Error codes for commands.
 enum {
 	APP_CMD_STATUS_OK = 0,			 	// All good.
-	APP_CMD_STATUS_PENDING = 1,			// Command is running.
-	APP_CMD_STATUS_BAD_CMD = 2,			// Unknown command.
-	APP_CMD_STATUS_RELAY_FAIL = 3,		// Relay module offline, cannot command motors.
-	APP_CMD_STATUS_PRESET_INVALID = 4,	// Preset in NV was invalid.
+	APP_CMD_STATUS_INACTIVE = 1,		// Controller not awake, command failed.
+	APP_CMD_STATUS_PENDING = 2,			// Command is running.
+	APP_CMD_STATUS_BAD_CMD = 3,			// Unknown command.
+	APP_CMD_STATUS_RELAY_FAIL = 4,		// Relay module offline, cannot command motors.
+	APP_CMD_STATUS_PRESET_INVALID = 5,	// Preset in NV was invalid.
+	APP_CMD_STATUS_SLEW_TIMEOUT = 6,	// Timeout on slew to position. 
 
 	APP_CMD_STATUS_SENSOR_FAIL_0 = 10,	// Tilt sensor 0 offline or failed.
 	APP_CMD_STATUS_SENSOR_FAIL_1 = 11,	// Tilt sensor 1 offline or failed.
 
 	APP_CMD_STATUS_NO_MOTION_0 = 20,	// No motion detected on sensor 0.
 	APP_CMD_STATUS_NO_MOTION_1 = 21,	// No motion detected on sensor 1.
-	APP_CMD_STATUS_SLEW_TIMEOUT = 22,	// Timeout on slew to position. 
 };
 
 #endif	// APP_H__
