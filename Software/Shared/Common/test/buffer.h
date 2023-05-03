df1f8e4c9f6bbecfb05bae7026e12575a21e8ce6
celephicus <celephicus@gmail.com>
2023-05-03 12:47:07 +1000

    Playing with Buffer class.

--- Software/Shared/Common/test/buffer.h
+++ Software/Shared/Common/test/buffer.h
@@ -38,4 +38,36 @@ public:
 	operator const uint8_t*() const { return m_buf; }
 };
 
+// Uses a static buffer and a pointer to next free location, which is NULL on overflow. 
+template <const size_t CAPACITY_>
+class SBuffer {
+	static constexpr m_size = CAPACITY_;
+	uint8_t m_buf[CAPACITY_];		
+	uint8_t* m_p;		// NULL on overflow.
+public:
+	SBuffer() { clear(); }
+	// SBuffer(const SBuffer<CAPACITY_>& b) { memcpy(m_buf, b.m_buf, b.len()); m_p = b.ovf() ? NULL : m_buf + b.len(); }
+	Buffer(const Buffer& b) = delete;
+	Buffer& operator = (const Buffer& rhs) = delete;
+	~Buffer() { }
+	void clear() { m_p = m_buf; }
+	uint8_t size() const { return m_size; }
+	uint8_t len() const { return ovf() ? m_size : m_p - m_buf; }	
+	uint8_t free() const { return ovf() ? 0 : m_buf + m_size - m_p; }	
+	bool ovf() const { return NULL != m_p; }
+	void add(uint8_t c) { if (free() > 0) *m_p++ = c; else m_p = NULL; }
+	void add(char c) { add((uint8_t)c); }
+	void add(const uint8_t* buf, uint8_t sz) { 
+	if (!ovf()) {
+		if (sz > free()) { sz = free(); 
+		memcpy(m_p, buf, sz); m_p += sz; 
+	}
+	void add(uint16_t u16) { 
+		add((const uint8_t*)&u16, sizeof(u16)); 
+	}
+	uint8_t operator [](uint8_t idx) const { return m_buf[idx]; }
+	operator const uint8_t*() const { return m_buf; }
+};
+
+
 #endif	// BUFFER_H__
