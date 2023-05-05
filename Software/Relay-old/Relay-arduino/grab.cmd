e69dda1059297ab5b9cbdc5bffe266046278908f
celephicus <celephicus@gmail.com>
2023-05-05 13:31:09 +1000

    Deleted old project.

--- Software/Relay-old/Relay-arduino/grab.cmd
+++
@@ -1,7 +0,0 @@
-robocopy ..\Relay-as\Relay-as . driver.h driver.cpp project_config.h regs_local.h gpio.h 
-copy ..\Relay-as\Relay-as\Relay-as.cpp Relay-arduino.ino
-
-robocopy ..\..\Shared\Common\include . console.h modbus.h regs.h sbc2022_modbus.h utils.h
-robocopy ..\..\Shared\Common\src . console.cpp modbus.cpp regs.cpp utils.cpp
-robocopy ..\..\Shared\AVR\include . dev.h 
-robocopy ..\..\Shared\AVR\src . dev.cpp 
--- Software/Relay-old/history/Relay-arduino/sm_supervisor.h
+++
@@ -1,9 +0,0 @@
-
-typedef struct {
-    EventSmContextBase base;
-    uint16_t timer_cookie[2]; 
-} SmSupervisorContext;
-
-int8_t smSupervisor(EventSmContextBase* context, event_t* ev);
-
-
